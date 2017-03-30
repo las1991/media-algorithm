9fd7bf9c64455152c931b109fc5656b42fe48800
liwei <lw@sengled.com>
2017-03-30 13:08:08 +0800

    dynamodb client 参数

--- src/java/src/main/java/com/sengled/mediaworker/dynamodb/Dynamodb.java
+++ src/java/src/main/java/com/sengled/mediaworker/dynamodb/Dynamodb.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.Protocol;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.internal.StaticCredentialsProvider;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 @Component
 public class Dynamodb {
@@ -24,17 +26,32 @@ public class Dynamodb {
 		
 	@Value("${AWS_DYNAMO_REGION}")
 	private String region;
-
+	
+	private ClientConfiguration getConfig() {
+		ClientConfiguration conf =  new ClientConfiguration();
+		conf.setUseTcpKeepAlive(true);
+		conf.setMaxErrorRetry(3);
+		conf.setConnectionTimeout(15*1000);
+		conf.setSocketTimeout(60*1000);
+		conf.setProtocol(Protocol.HTTPS);
+		conf.setMaxConnections(3 * ClientConfiguration.DEFAULT_MAX_CONNECTIONS);
+		conf.setUseTcpKeepAlive(true);
+		return conf;
+	}
 	
 	@Bean
 	public DynamodbTemplate getDynamodbTemplate(){
 		AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 		AWSCredentialsProvider credentialsProvider = new StaticCredentialsProvider(credentials);
-		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+		AmazonDynamoDBAsync client = AmazonDynamoDBAsyncClientBuilder.standard()
 				.withCredentials(credentialsProvider)
 				.withRegion(region)
+				.withClientConfiguration(getConfig())
 				.build();
 		DynamoDB dynamoDB = new DynamoDB(client);
 		return new DynamodbTemplate(dynamoDB);
 	}
+
+
+
 }
