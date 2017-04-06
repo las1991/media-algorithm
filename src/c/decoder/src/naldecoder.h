ebf59a2a342ff734cc655646c678902d817b4cf1
pangerping <pep@sengled.com>
2017-04-06 15:49:14 +0800

    remove const

--- src/c/decoder/src/naldecoder.h
+++ src/c/decoder/src/naldecoder.h
@@ -5,13 +5,13 @@ typedef struct YUVFrame
 {
     int   width;
     int   height;
-	char* data;
-	int   size;
+    char* data;
+    int   size;
 }YUVFrame;
 
 int Init(void (*log_callback )(int level, char* ));
 
-int DecodeNal(const char* data_buffer, int len, const char* token, YUVFrame* yuv_frame);
+int DecodeNal(char* data_buffer, int len, const char* token, YUVFrame* yuv_frame);
 
 int Destroy(YUVFrame* yuv_frame);
 
