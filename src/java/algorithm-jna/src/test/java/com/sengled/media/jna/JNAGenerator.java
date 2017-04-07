d7ed2c95ce9a74a689c56bc9c257dc06eba84a33
chenxh <cxh@sengled.com>
2017-04-07 16:20:30 +0800

    jna 自动构造

--- src/java/algorithm-jna/src/test/java/com/sengled/media/jna/JNAGenerator.java
+++ src/java/algorithm-jna/src/test/java/com/sengled/media/jna/JNAGenerator.java
@@ -3,6 +3,7 @@ package com.sengled.media.jna;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 import com.ochafik.lang.jnaerator.JNAerator;
 import com.sun.jna.Platform;
@@ -23,42 +24,46 @@ public static void main(String[] args) throws IOException {
         soFile.createNewFile();
     }
     
-    
+    Files.copy(new File("../../c/decoder/src/nal_decoder.h").toPath(), new File("./nal_decoder.h").toPath(), StandardCopyOption.REPLACE_EXISTING);
+    Files.copy(new File("../../c/encoder/src/jpg_encoder.h").toPath(), new File("./jpg_encoder.h").toPath(), StandardCopyOption.REPLACE_EXISTING);
+    Files.copy(new File("../../c/motion/include/sengled_algorithm.h").toPath(), new File("./sengled_algorithm.h").toPath(), StandardCopyOption.REPLACE_EXISTING);
+    Files.copy(new File("../../c/common/log.h").toPath(), new File("./log.h").toPath(), StandardCopyOption.REPLACE_EXISTING);
+    Files.copy(new File("../../c/common/yuv.h").toPath(), new File("./yuv.h").toPath(), StandardCopyOption.REPLACE_EXISTING);
     /**
-     * 解码库
+     * 图片压缩库
      */
     JNAerator.main(new String[]{
             soFile.getAbsolutePath(), 
-            "../../c/decoder/src/naldecoder.h", 
+            "./jpg_encoder.h", 
             "-mode", "Directory", 
             "-runtime", "JNA", 
             "-root", "com.sengled.media.jna", 
             "-o", "src/main/java", 
             "-f"});
+    
     
     /**
      * 图片压缩库
      */
     JNAerator.main(new String[]{
             soFile.getAbsolutePath(), 
-            "../../c/encoder/src/yuvencoder.h", 
+            "./nal_decoder.h", 
             "-mode", "Directory", 
             "-runtime", "JNA", 
             "-root", "com.sengled.media.jna", 
             "-o", "src/main/java", 
-            "-f"});
-    
-    
+            "-f"});    
     /**
-     * motion 检测算法库
+     * 图片压缩库
      */
     JNAerator.main(new String[]{
             soFile.getAbsolutePath(), 
-            "../../c/motion/include/sengled_algorithm.h", 
+            "./sengled_algorithm.h", 
             "-mode", "Directory", 
             "-runtime", "JNA", 
             "-root", "com.sengled.media.jna", 
             "-o", "src/main/java", 
             "-f"});
+    
 }
 }
