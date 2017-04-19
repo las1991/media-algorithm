9401ebde47cdbe924382adfd94a18934993fa828
Wang Xiu Chuan <wxc@sengled.com>
2017-04-18 20:45:53 -0700

    add json paraser

--- src/c/motion/main/AlgorithmManager.cpp
+++ src/c/motion/main/AlgorithmManager.cpp
@@ -1,6 +1,6 @@
 #include "AlgorithmManager.h"
 #include "motionAction.h"
-
+#include "cJSON.h"
 void LocalPrint( void* context )
 {
 	rvResource *rv = static_cast<rvResource*>(context);
@@ -28,60 +28,69 @@ void LocalPrint( void* context )
 
 void CopyAlgorithmParams( rvResource *rv, void *algorithm_param )
 {
-    SLS_AlgorithmParams *pParams = (SLS_AlgorithmParams*)algorithm_param;
-	rv->pAlgoParams->motion_params.zone_count = pParams->motion_params.zone_count;
-	int zone_count = rv->pAlgoParams->motion_params.zone_count;
-	for ( int i = 0 ; i < zone_count; i++ )
-	{
-        rv->pAlgoParams->motion_params.zone_id[i] = pParams->motion_params.zone_id[i];
-        rv->pAlgoParams->motion_params.zone_pos[i] = pParams->motion_params.zone_pos[i];
-	}
-	rv->pAlgoParams->motion_setting_params =  pParams->motion_setting_params;
-}
-/*
-bool CompareRect( SLS_RoiRect &last_rect, SLS_RoiRect &cur_rect )
-{
-	return last_rect.x != cur_rect.x ? false : 
-		last_rect.y != cur_rect.y ? false :
-		last_rect.width != cur_rect.width ? false :
-		last_rect.height != cur_rect.height ? false :
-		true;
-}
+    char *json_message = (char*)algorithm_param;
+    rv->plog->log_print(SLS_LOG_DEBUG,"params=%s",json_message);
 
-bool CompareCommonParams( SLS_CommonSettingParams &last_param, SLS_CommonSettingParams &cur_param )
-{
-	return last_param.frame_skip != cur_param.frame_skip ? false :
-		last_param.sensitivity != cur_param.sensitivity ? false :
-		last_param.sleep_period != cur_param.sleep_period ? false :
-		true;
-}
+    cJSON *pjson = cJSON_Parse(json_message);
+    if( pjson == NULL )
+    {
+        rv->plog->log_print(SLS_LOG_ERROR,"parse json error");
+        return;
+    }
+    cJSON *psensi = cJSON_GetObjectItem(pjson,"sensitivity");
+    if( psensi == NULL )
+    {
+        rv->plog->log_print(SLS_LOG_ERROR,"parse json sensitivity error");
+        return;
+    }
+	rv->pAlgoParams->motion_setting_params.sensitivity = psensi->valueint;
 
-bool IsMotionParamsUpdate( rvResource *rv, void *algorithm_param )
-{
-     SLS_AlgorithmParams *pParams = (SLS_AlgorithmParams*)algorithm_param;
-	 int zone_cont = pParams->motion_params.zone_count;
-	 
-	 if( zone_cont != rv->pAlgoParams->motion_params.zone_count )
-	 {
-		 rv->plog->log_print( "%s--zone count has been updated",rv->alocontext.token );
-		 return true;
-	 }
-	 for ( int i = 0; i < zone_cont; i++ )
-	 {
-		 if( !CompareRect( rv->pAlgoParams->motion_params.zone_pos[i], pParams->motion_params.zone_pos[i] ) )
-		 {
-			 rv->plog->log_print( "%s--zone pos has been updated", rv->alocontext.token );
-			 return true;
-		 }
-	 }
-	 if ( !CompareCommonParams(rv->pAlgoParams->motion_setting_params,pParams->motion_setting_params) )
-	 {
-		 rv->plog->log_print( "%s---motions seting params has been updated ", rv->alocontext.token );
-		 return true;
-	 }
-	 return false;
+    cJSON *pparam_array= cJSON_GetObjectItem(pjson,"dataList");
+    if(pparam_array == NULL || cJSON_Array != pparam_array->type)
+    {
+        rv->plog->log_print(SLS_LOG_ERROR,"parse json dataList error");
+        return;
+    } 
+    int size = cJSON_GetArraySize(pparam_array);
+    if(size > 3 )
+    {
+        rv->plog->log_print(SLS_LOG_ERROR,"params number overflow");
+        return;
+    }
+	rv->pAlgoParams->motion_params.zone_count = size;
+    for( int i = 0; i < size; i++ )
+    {
+        cJSON *item = cJSON_GetArrayItem(pparam_array,i);
+        if( item==NULL ) return;
+        cJSON *subitem = item->child;
+        if(subitem == NULL || subitem->next == NULL )
+        {
+            rv->plog->log_print(SLS_LOG_ERROR,"parse dataList element error");
+            return;
+        }
+        rv->plog->log_print(SLS_LOG_DEBUG,"%s:%d\n",subitem->string,subitem->valueint);
+        rv->pAlgoParams->motion_params.zone_id[i] = subitem->valueint;
+
+        rv->plog->log_print(SLS_LOG_DEBUG,"%s:%s\n",subitem->next->string,subitem->next->valuestring);
+        char *str = subitem->next->valuestring;
+        char *token = strtok(str,",");
+        int z_pos[4]={0},count=0;
+        while( token != NULL && count < 4)
+        {
+            int p = atoi(token);
+            rv->plog->log_print(SLS_LOG_DEBUG,"pos=%d\n",p);
+            z_pos[count]=p;
+            token = strtok(NULL,",");
+            count++;
+        }
+        rv->pAlgoParams->motion_params.zone_pos[i].x = z_pos[0];
+        rv->pAlgoParams->motion_params.zone_pos[i].y = z_pos[1];
+        rv->pAlgoParams->motion_params.zone_pos[i].width = z_pos[2];
+        rv->pAlgoParams->motion_params.zone_pos[i].height = z_pos[3];
+    }
+    cJSON_Delete(pjson);
+     
 }
-*/
 
 void DispatchAlgorithm( rvResource *rv,void *frame,int frame_width,int frame_height,
         void *algorithm_param, algorithm_result *result )
