9a932225728cc3c1cd603b38acdd5a840a128a9e
las <las@sengled.com>
2018-10-29 18:12:23 +0800

    ffmpeg 编译增加参数--disable-podpages --disable-doc

--- src/c/3rd/build.sh
+++ src/c/3rd/build.sh
@@ -6,7 +6,7 @@ chmod +x *.sh
 make clean
 make distclean
 #--extra-cflags="-fvisibility=hidden" 
-./configure  --prefix="./../install" --enable-gpl  --disable-shared --enable-static --enable-nonfree --enable-pic  --enable-decoder=h264 --enable-swscale  --enable-swresample --disable-optimizations --disable-stripping  --enable-debug
+./configure  --prefix="./../install" --enable-gpl  --disable-shared --enable-static --enable-nonfree --enable-pic  --enable-decoder=h264 --enable-swscale  --enable-swresample --disable-optimizations --disable-stripping  --enable-debug --disable-podpages --disable-doc
 
 make -j4 install
 
